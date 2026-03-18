369b551e757359d806c1fb5f1564c23ae2491533
narukami-22 <lohitakshrudra@gmail.com>
2026-03-18 08:46:51 +0530

    Delete simulate directory

--- simulate/test_model.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-model = tf.keras.models.load_model("plant_model.keras")
-
-sample = np.array([[0.20, 0.60, 0.70, 0.50]])
-prediction = model.predict(sample)
-print("Prediction:", prediction.argmax())
